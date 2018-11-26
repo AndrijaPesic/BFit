771c83e60aa89550c87229e35525413b5f9bd34e
Andrija Pesic <andrijapesic21@gmail.com>
2018-11-26 20:59:06 +0100

    Added NoActionBar Theme, tried to use Log.e() logger.

--- app/src/main/java/rs/elfak/mosis/akitoske/bfit/LoginActivity.java
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/LoginActivity.java
@@ -7,8 +7,9 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
