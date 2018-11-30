f1959fccd16a59812bfc89afdee76954889aa43f
Andrija Pesic <andrijapesic21@gmail.com>
2018-11-30 18:00:31 +0100

    Added working LoginActivity

--- app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/LoginActivity.java
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/LoginActivity.java
@@ -1,16 +1,25 @@
 package rs.elfak.mosis.akitoske.bfit.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 import rs.elfak.mosis.akitoske.bfit.R;
+import rs.elfak.mosis.akitoske.bfit.utils.Validator;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnFocusChangeListener {
 
     private EditText mEmail;
     private EditText mPassword;
@@ -19,14 +28,72 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mEmail = findViewById(R.id.login_email_text);
+        mPassword = findViewById(R.id.login_password_text);
+
+        mEmail.setOnFocusChangeListener(this);
+        mPassword.setOnFocusChangeListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
-    public void onLoginClick(View view){
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if (!hasFocus) {
+            EditText editText = (EditText) view;
+            int editTextId = editText.getId();
 
+            switch (editTextId) {
+                case R.id.login_email_text:
+                    Validator.validateEmail(editText);
+                    break;
+            }
+        }
+    }
+
+    private boolean allFieldsValid() {
+        Validator.validateEmail(mEmail);
+        return mEmail.length() > 0 && mEmail.getError() == null &&
+                mPassword.length() > 0 && mPassword.getError() == null;
+    }
+
+    public void onLoginClick(View view) {
+        if (!allFieldsValid()) return;
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getString(R.string.login_progress_dialog_message));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthInvalidUserException ex) {
+                                mEmail.setError(getString(R.string.login_email_not_exist_error));
+                            } catch (FirebaseAuthInvalidCredentialsException ex) {
+                                mPassword.setError(getString(R.string.login_wrong_password_error));
+                            } catch (Exception e) {
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
     }
 
     public void onNoAccountClick(View v) {
