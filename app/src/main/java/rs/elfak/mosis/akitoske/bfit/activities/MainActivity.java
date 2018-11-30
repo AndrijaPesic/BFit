b77807b92c065443a9fed320680851fc80a1c711
Andrija Pesic <andrijapesic21@gmail.com>
2018-11-30 19:58:45 +0100

    Added main-menu + updated services and all versions to run google maps. Added map fragment. Changed some styles and emptied MainActivity

--- app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/MainActivity.java
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/MainActivity.java
@@ -2,6 +2,9 @@ package rs.elfak.mosis.akitoske.bfit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 import rs.elfak.mosis.akitoske.bfit.R;
 
@@ -13,4 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
---
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/fragments/MapFragment.java
@@ -0,0 +1,5 @@
+package rs.elfak.mosis.akitoske.bfit.fragments;
+
+public class MapFragment {
+
+}
