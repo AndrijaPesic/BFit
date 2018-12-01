ba76bd473fca21d14c532f6d7cbbc44b35c1260f
Nikola Tosic <nikolatosic@zenchain.com>
2018-12-01 02:02:39 +0100

    Added action bar

--- app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/LauncherActivity.java
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/LauncherActivity.java
@@ -1,4 +1,24 @@
 package rs.elfak.mosis.akitoske.bfit.activities;
 
-public class LauncherActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        boolean loggedIn = FirebaseAuth.getInstance().getCurrentUser() != null;
+
+        if (!loggedIn) {
+            startActivity(new Intent(LauncherActivity.this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(LauncherActivity.this, MainActivity.class));
+        }
+        finish();
+    }
 }
--- app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/MainActivity.java
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/activities/MainActivity.java
@@ -1,19 +1,28 @@
 package rs.elfak.mosis.akitoske.bfit.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
 
 import rs.elfak.mosis.akitoske.bfit.R;
+import rs.elfak.mosis.akitoske.bfit.providers.FirebaseProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 
@@ -22,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.action_bar_main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_bar_logout_item:
+                FirebaseProvider.getInstance().getAuthInstance().signOut();
+                Intent logoutIntent = new Intent(MainActivity.this, LauncherActivity.class);
+                startActivity(logoutIntent);
+                finish();
+                return true;
+        }
+
+        // If none of the 'case' statements return true, we return false to let a specific fragment handle the option
+        return false;
     }
 
 }
