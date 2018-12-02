3b49c3b0d8302c35cecdfd8e5faaac7f2b4ea3f1
Andrija Pesic <andrijapesic21@gmail.com>
2018-12-02 18:31:38 +0100

    Added Map and NoLocation fragments, location detection fully works and map loads.

--- app/src/main/java/rs/elfak/mosis/akitoske/bfit/fragments/MapFragment.java
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/fragments/MapFragment.java
@@ -1,5 +1,114 @@
 package rs.elfak.mosis.akitoske.bfit.fragments;
 
-public class MapFragment {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import rs.elfak.mosis.akitoske.bfit.R;
+import rs.elfak.mosis.akitoske.bfit.activities.MainActivity;
+
+public class MapFragment extends BaseFragment implements OnMapReadyCallback{
+
+    public static final String FRAGMENT_TAG = "MapFragment";
+
+    private Context mContext;
+    private MapView mMapView;
+    private Map<Marker, GoogleMap.OnMarkerClickListener> mMarkerListeners = new HashMap<>();
+    private GoogleMap mGoogleMap;
+    private OnFragmentInteractionListener mListener;
+
+    public interface OnFragmentInteractionListener {
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setActionBarTitle(null);
+        getActivity().findViewById(R.id.toolbar_filter_spinner).setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View inflatedView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mMapView = (MapView) inflatedView.findViewById(R.id.map_fragment_map_view);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
+        return inflatedView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        mGoogleMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.style_json)));
+
+        try {
+            mGoogleMap.setMyLocationEnabled(true);
+        } catch (SecurityException e) {
+            // We're not handling the exception here because we receive locations from the service
+            // that will only be enabled if we have location permission
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+
+        mContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+        mContext = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
 
 }
---
+++ app/src/main/java/rs/elfak/mosis/akitoske/bfit/fragments/NoLocationFragment.java
@@ -0,0 +1,85 @@
+package rs.elfak.mosis.akitoske.bfit.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import rs.elfak.mosis.akitoske.bfit.R;
+
+public class NoLocationFragment extends BaseFragment implements View.OnClickListener {
+
+    public static final String FRAGMENT_TAG = "NoLocationFragment";
+
+    private OnFragmentInteractionListener mListener;
+
+    public interface OnFragmentInteractionListener {
+        void onNoLocationContinueClick();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setActionBarTitle(null);
+        getActivity().findViewById(R.id.toolbar_filter_spinner).setVisibility(View.INVISIBLE);
+    }
+
+    public NoLocationFragment() {
+        // Required empty public constructor
+    }
+
+    public static NoLocationFragment newInstance() {
+        NoLocationFragment fragment = new NoLocationFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_no_location, container, false);
+
+        Button continueButton = (Button) view.findViewById(R.id.no_location_continue_btn);
+        continueButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mListener != null) {
+            mListener.onNoLocationContinueClick();
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.setGroupVisible(R.id.main_menu_group, false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
